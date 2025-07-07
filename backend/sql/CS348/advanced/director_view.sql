d2cc340a37de9593a8f3cc6de744b760e665eff6
Anantjyot Grang <68477945+granganantjyot@users.noreply.github.com>
2025-07-07 15:04:27 -0400

    Update director_view.sql

--- backend/sql/CS348/advanced/director_view.sql
+++ backend/sql/CS348/advanced/director_view.sql
@@ -4,7 +4,7 @@ CREATE VIEW v_top_directors AS
 SELECT d.director_id,
     d.name,
     ROUND(AVG(r.rating),2) AS avg_rating,
-    COUNT() AS film_count
+    COUNT(DISTINCT r.movie_id) AS film_count
 FROM directors d
 JOIN movie_directors md USING (director_id)
 JOIN reviews r USING (movie_id)
